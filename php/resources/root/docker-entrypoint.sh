c435ec5fcce8d0dc80b2caa743adb5e1946fce9f
Eric Poe <eric.poe@gmail.com>
2017-09-25 16:51:05 -0500

    Remove the running of symfony scripts
    These can be launched via external scripts against the container

--- php/resources/root/docker-entrypoint.sh
+++ php/resources/root/docker-entrypoint.sh
@@ -33,19 +33,8 @@ echo "Switching to UID ${DOCKERLIMS_SHELL_UID}"
 usermod -u ${DOCKERLIMS_SHELL_UID} limsuser
 groupmod -g ${DOCKERLIMS_SHELL_UID} limsuser
 
-
 # Ensure limsuser will have permissions on all lims-related files
 chown -R limsuser:limsuser /var/lims/
-
-# This will install dependencies. --no-scripts is necessary because building
-# the bootstrap file is another step
-
-#todo: enable --no-scripts when PR 194 is merged
-#su -p limsuser -c '/var/www/html/Symfony/bin/composer.phar install --no-scripts'
-
-# build bootstrap file
-# re-enable when this PR is merged: https://bitbucket.stowers.org/projects/LIMS/repos/symfony/pull-requests/194/overview
-#su -p limsuser -c '/var/www/html/Symfony/bin/rebuild-bootstrap.sh'
 
 #
 # Start apache in the foreground as the limsuser

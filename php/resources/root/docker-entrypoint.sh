e19a67597e2e6c7e60d514f08b48d2e34f964e80
Eric Poe <eric.poe@gmail.com>
2017-09-26 10:49:39 -0500

    Remove unused MySQL config
    This should be set in the docker-compose.yml or in the long docker-run
    command.

--- php/resources/root/docker-entrypoint.sh
+++ php/resources/root/docker-entrypoint.sh
@@ -9,24 +9,6 @@ onTerminate() {
 }
 trap 'onTerminate' SIGTERM
 
-#
-# Ensure database is set up correctly
-#
-
-# Wait for database to be started...
-# until echo "show tables" | mysql -h db -u limsdbuser --password='limsdbpassword' limsdockerdb &> /dev/null
-# do
-#     echo "Waiting for mysql..."
-#     sleep 2
-# done
-
-# mysql -u root --password='root' -h db < /var/lims/setup/legacy-setup.sql
-# mysql -u root --password='root' -h db < /var/lims/setup/setup.sql
-
-#
-# Initialize container-specific files
-#
-
 # Ensure limsuser uid and gid matches environment configuration (see docker-compose.yml)
 : ${DOCKERLIMS_SHELL_UID:=5000}
 echo "Switching to UID ${DOCKERLIMS_SHELL_UID}"
